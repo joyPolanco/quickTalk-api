a552f3eac7942eaf19246f5c45b8878bb0468043
Johaly <johalypolanco13@gmail.com>
2026-05-05 22:16:51 -0400

    deploy

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,12 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.resolve("config/firebase.json"))
-);
+const serviceAccount = {
+  project_id: process.env.FIREBASE_JSON_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_JSON_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_JSON_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_JSON_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_JSON_CLIENT_ID,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
