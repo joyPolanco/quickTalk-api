cc5010b598c128fa307998e73349505208477d11
Johaly <johalypolanco13@gmail.com>
2026-05-05 22:31:06 -0400

    deploy

--- config/firebase.js
+++ config/firebase.js
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 const serviceAccount = {
   project_id: process.env.FIREBASE_JSON_PROJECT_ID,
   private_key_id: process.env.FIREBASE_JSON_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_JSON_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  private_key: process.env.FIREBASE_JSON_PRIVATE_KEY,
   client_email: process.env.FIREBASE_JSON_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_JSON_CLIENT_ID,
 };
